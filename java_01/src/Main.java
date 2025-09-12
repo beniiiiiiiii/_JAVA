424bb3c689cd5dffaa6eeee532239913f79af561
benedek <csombovargabenedek@gmail.com>
2025-09-12 08:42:35 +0200

    java 01

---
+++ java_01/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World" + "1");
+        System.out.println("sup g");
+
+        int x = 5;
+        System.out.println(x);
+
+        double y = 5.78;
+
+        String szoveg = "alma";
+        System.out.println("szoveg = " + szoveg);
+
+        x = 1;
+        System.out.println(x++);
+        System.out.println(++x);
+    }
+}
